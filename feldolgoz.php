fb92a60c76e5e29223ee1197f6c42e6da961c256
master <brainermaister3@gmail.com>
2012-12-05 18:42:37 +0100

    rezgo

---
+++ feldolgoz.php
@@ -0,0 +1,50 @@
+﻿<html>
+	<head>
+		<title>R-L-C Soros rezgő kör</title>
+	</head>
+	<body>
+<?php
+$U = $_POST['U'];
+$C = $_POST['C'];
+$L = $_POST['L'];
+$R = $_POST['R'];
+$cm = $_POST['cm'];
+$lm = $_POST['lm'];
+if(empty($U) or empty($C) or empty($L) or empty($R)){header("Location:index.php?hiba=1");exit;}
+else{
+	echo"U=$U V</br>C=$C $cm</br>L=$L $lm</br>R=$R Ohm</br></br>";
+	switch($cm){
+		case 'mF': $C=$C/1000;break;
+		case 'uF': $C=$C/1000000;break;
+		case 'nF': $C=$C/1000000000;break;
+		case 'pF': $C=$C/1000000000000;break;
+	}
+	switch($lm){
+		case 'mH': $L=$L/1000;break;
+		case 'uH': $L=$L/1000000;break;
+		case 'nH': $L=$L/1000000000;break;
+		case 'pH': $L=$L/1000000000000;break;
+	}
+	echo "Rezonancia körfrekvencia</br>";
+		$w=1/sqrt($L*$C);
+		echo "w=1/sqrt(L*C)=1/sqrt($L*$C)=$w cps</br></br>";
+	echo "Rezonancia frekvencia</br>";
+		$fr=$w/(2*Pi());
+		echo "fr=w/(2*Pi)=$w/(2*Pi)=$fr fok</br></br>";
+	echo "Körjóság</br>";
+		$Q0=1/($R*sqrt($L/$C));
+		echo "Q0=1/(R*sqrt)(L/C)=1/($R*sqrt($L/$C))=$Q0</br></br>";
+	echo "Sávszélesség</br>";
+		$B=$fr/$Q0;
+		echo "B=fr/Q0=$fr/$Q0=$B Hz</br></br>";
+	echo "Áram rezonancián</br>";
+		$I0=$U/$R;
+		echo "I0=U/R=$U/$R=$I0 A</br></br>";
+	echo "Kondenzátor reaktanciája</br>";
+		$Xc0=1/($w*$C);
+		echo "Xc0=1/(w*C)=1/($w*$C)=$Xc0 Ohm</br></br>";
+	
+}
+?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,33 @@
+﻿<html>
+	<head>
+		<title>R-L-C Soros rezgő kör</title>
+	</head>
+	<body>
+		<form method="POST" action="feldolgoz.php">
+			Adja meg a Generáor feszültségét Volt-ban!    (U)</br>
+			<input type="text" name="U">V</br>
+			Adja meg a Kondenzátor kapacitását Farad-ban! (C)</br>
+			<input type="text" name="C"><select name="cm">
+											<option>mF</option>
+											<option>uF</option>
+											<option>nF</option>
+											<option>pF</option>
+										</select></br>
+			Adja meg a Tekercs induktivitását Henry-ben!  (L)</br>
+			<input type="text" name="L"><select name="lm">
+											<option>mH</option>
+											<option>uH</option>
+											<option>nH</option>
+											<option>pH</option>
+										</select></br>
+			Adja meg az Ellenállás ellenállását Ohm-ban!  (R)</br>
+			<input type="text" name="R">Ohm</br>
+			<input type="submit" value="Számítás"><br/>
+			<?php
+				if(isset($_GET['hiba'])){$hiba = $_GET['hiba'];}
+				else{$hiba='';}
+				if($hiba == 1){echo"<div>Hiba, kitöltetlen mezők!</div>";}
+			?>
+		</form>
+	</body>
+</html>
