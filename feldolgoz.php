724ec3741d109cc8fc2f1a1d6ca8b1485cf3eb6a
master <brainermaister3@gmail.com>
2012-12-08 17:22:25 +0100

    new encodeing & optimalized functions

--- feldolgoz.php
+++ feldolgoz.php
@@ -5,36 +5,42 @@
 	<body>
 <?php
 function normal($be){
-	if ($be>=1000000){$be=$be/1000000;$me='M';$exp='*10^6';}
-	elseif ($be>=1000){$be=$be/1000;$me='K';$exp='*10^3';}
-	elseif ($be<0.000000001){$be=$be*1000000000;$me='p';$exp='*10^-12';}
-	elseif ($be<0.000001){$be=$be*1000000;$me='n';$exp='*10^-9';}
-	elseif ($be<0.001){$be=$be*1000;$me='µ';$exp='*10^-6';}
-	elseif ($be<1){$be=$be*1000;$me='m';$exp='*10^-3';}
+	if ($be>=1E6){$be=$be*1E-6;$me='M';$exp='*10^6';}
+	elseif ($be>=1E3){$be=$be*1E-3;$me='K';$exp='*10^3';}
+	elseif ($be<1E-9){$be=$be*1E9;$me='p';$exp='*10^-12';}
+	elseif ($be<1E-6){$be=$be*1E6;$me='n';$exp='*10^-9';}
+	elseif ($be<1E-3){$be=$be*1E3;$me='µ';$exp='*10^-6';}
+	elseif ($be<1){$be=$be*1E3;$me='m';$exp='*10^-3';}
 	else{$me='';$exp='';}
 	return array(round($be,2),$me,$exp);
 }
 function mert($sz, $ki){
-	switch(utf8_encode($ki[1])){
-		case 'M': $sz=$sz*1000000;break;
-		case 'K': $sz=$sz*1000;break;
-		case 'm': $sz=$sz/1000;break;
-		case 'µ': $sz=$sz/1000000;break;
-		case 'n': $sz=$sz/1000000000;break;
-		case 'p': $sz=$sz/1000000000000;break;}
+	$k=0;
+	for($i=0;$i<=strlen($ki)-1;$i++){
+		if($ki[$i]==utf8_encode($ki[$i])){$utf[$k]=$ki[$i];$k=$k+1;}
+		elseif($ki[$i]!=utf8_encode($ki[$i]) && $ki[$i+1]!=utf8_encode($ki[$i+1]))
+			{$i=$i+1;$utf[$k]=utf8_encode($ki[$i]);$k=$k+1;}}
+	switch($utf[0]){
+		case 'M': $sz=$sz*(1E6);break;
+		case 'K': $sz=$sz*(1E3);break;
+		case 'm': $sz=$sz*(1E-3);break;
+		case 'µ': $sz=$sz*(1E-6);break;
+		case 'n': $sz=$sz*(1E-9);break;
+		case 'p': $sz=$sz*(1E-12);break;
+		default:break;}
 	return $sz;
 }
 
 $U = $_POST['U'];
-$C = $_POST['C'];
-$L = $_POST['L'];
 $R = $_POST['R'];
+$L = $_POST['L'];
+$C = $_POST['C'];
 $rm = $_POST['rm'];
-$cm = $_POST['cm'];
 $lm = $_POST['lm'];
+$cm = $_POST['cm'];
 if(empty($U) or empty($C) or empty($L) or empty($R)){header("Location:index.php?hiba=1");exit;}
 else{
-	echo"R-L-C Soros rezgő kör©</br>U=$U V</br>C=$C $cm</br>L=$L $lm</br>R=$R Ohm</br></br>";
+	echo "R-L-C Soros rezgő kör©</br>U=$U V</br>C=$C $cm</br>L=$L $lm</br>R=$R $rm</br></br>";
 	$R=mert($R, $rm);
 	$L=mert($L, $lm);
 	$C=mert($C, $cm);
